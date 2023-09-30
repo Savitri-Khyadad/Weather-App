a38f743c6560becf77411cf88102b06ee8faf550
SavitriKhyadad <savitrikhyadad15@gmail.com>
2023-09-30 21:42:20 +0530

    Changed the look of the page. Need to add routing

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -1,95 +1,100 @@
-import React, {useState } from 'react';
-import Auxiliry from '../../hoc/Auxiliary/Auxiliary';
-import classes from './Layout.css';
-import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
-import SideDrawer from '../../components/Navigation/Sidedrawer/SideDrawer';
-import Modal from '../../components/UI/Modal/Modal';
-//import 
+import React, { useState } from "react";
+import Auxiliry from "../../hoc/Auxiliary/Auxiliary";
+import classes from "./Layout.css";
+import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
+import SideDrawer from "../../components/Navigation/Sidedrawer/SideDrawer";
+import Modal from "../../components/UI/Modal/Modal";
+//import
 
 const layout = () => {
+  const api = {
+    key: process.env.REACT_APP_WEATHER_API_KEY,
+    base: "https://api.openweathermap.org/data/2.5/",
+  };
+  const [showSideDrawer, setShowSideDrawer] = useState(true);
+  const [showWeather, setShowWeather] = useState(false);
+  const [search, setSearch] = useState("");
+  const [weather, setWeather] = useState({});
+  const [temp, setTemp] = useState("");
 
-    const api = {
-        key:process.env.REACT_APP_WEATHER_API_KEY,
-        base:"https://api.openweathermap.org/data/2.5/"
+  const select = (props) => {
+    if (props.key === "Enter") {
+      fetch(`${api.base}weather?q=${search}&units=metric&appid=${api.key}`)
+        .then((response) => response.json())
+        .then((res) => {
+          setSearch("");
+          setWeather(res);
+          setTemp(res.main.temp);
+          setShowWeather(true);
+        });
     }
-    const [showSideDrawer, setShowSideDrawer] = useState(true);
-    const [showWeather, setShowWeather] = useState(false);
-    const [search, setSearch] = useState('');
-    const [weather, setWeather] = useState({});
-    const [temp,setTemp] = useState('');
+  };
 
-    const select = (props) => {
-        if(props.key === "Enter") {
-            fetch(`${api.base}weather?q=${search}&units=metric&appid=${api.key}`)
-            .then(response => response.json())
-            .then(res => {
-                setSearch('');
-                setWeather(res);
-                setTemp(res.main.temp);
-                setShowWeather(true)
-            });
-        }
-    }
+  const modalCloseHandler = () => {
+    setShowWeather(false);
+  };
 
-    const modalCloseHandler = () => {
-        setShowWeather(false);
-    }
+  const showModal = () => {
+    setShowWeather(true);
+  };
 
-    const showModal = () => {
-        setShowWeather(true);
-    }
+  const sideDrawerClosedHandler = () => {
+    setShowSideDrawer(false);
+  };
 
-    const  sideDrawerClosedHandler = () => {
-        setShowSideDrawer(false);
-    }
+  const sideDrawerToggleHandler = () => {
+    let status = showSideDrawer;
+    setShowSideDrawer(!status);
+  };
 
-    const sideDrawerToggleHandler = () => {
-        let status = showSideDrawer;
-        setShowSideDrawer(!status)
-    }
+  return (
+    <Auxiliry>
+      <Toolbar drawerToggleClicked={sideDrawerToggleHandler} />
+      <SideDrawer open={showSideDrawer} closed={sideDrawerClosedHandler} />
+      <div className={classes.HeroImage}>
+        <div className={classes.container}>
+          <h1>
+            <span>WEATHER</span>
+          </h1>
+          <span className={classes.subtext}>Get current weather forecast</span>
+          <div className={classes.searchPart}>
+            <input
+              type="text"
+              placeholder="Search..."
+              className={classes.searchBar}
+              onChange={(event) => setSearch(event.target.value)}
+              value={search}
+              onKeyPress={select}
+            />
+          </div>
 
-    return (
-        <Auxiliry>
-            <Toolbar drawerToggleClicked={sideDrawerToggleHandler}/>
-            <SideDrawer open={showSideDrawer} closed={sideDrawerClosedHandler}/>
-            <div className={classes.HeroImage}>
-                <div className={classes.container}>
-                    <h1><span>WEATHER</span></h1>
-                    <span className={classes.subtext}>We provide current weather forecast</span>
-                        <div className={classes.searchPart}>
-                            <input type='text' placeholder='Search...' className={classes.searchBar} 
-                            onChange={event => setSearch(event.target.value)}
-                            value={search}
-                            onKeyPress={select}
-                            
-                           />
-                        </div>
-
-                        <button onClick={showModal}  className={classes.Button}>ENTER</button>
+          <button onClick={showModal} className={classes.Button}>
+            ENTER
+          </button>
+        </div>
+        {typeof weather !== "undefined" ? (
+          <div>
+            <div>
+              <Modal show={showWeather} modalClosed={modalCloseHandler}>
+                <div className={classes.display}>
+                  <div>{weather.name}</div>
+                  <div>{Math.round(temp)}°C</div>
                 </div>
-                {(typeof weather !== "undefined") ? (
-                    <div>
-                    <div>
-                        <Modal show={showWeather} modalClosed={modalCloseHandler}>
-                            <div className={classes.display}>
-                                <div>{weather.name}</div>
-                                <div>{Math.round(temp)}°C</div>
-                            </div>
-                        </Modal>
-                    </div>
-                    </div>
-                 ) : (
-                    <div>
-                    <Modal show={showWeather} modalClosed={modalCloseHandler}>
-                        <div className={classes.display}>
-                            <p>SORRY</p>
-                            <p>NO SUCH CITY FOUND!</p>
-                        </div>
-                    </Modal>
-                </div>
-                 )}
+              </Modal>
             </div>
-        </Auxiliry>
-    );
-}
+          </div>
+        ) : (
+          <div>
+            <Modal show={showWeather} modalClosed={modalCloseHandler}>
+              <div className={classes.display}>
+                <p>SORRY</p>
+                <p>NO SUCH CITY FOUND!</p>
+              </div>
+            </Modal>
+          </div>
+        )}
+      </div>
+    </Auxiliry>
+  );
+};
 export default layout;
